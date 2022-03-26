e59ada6030ffc7d4069613a3e65183a00abf6b11
ThusharM <79003269+ThusharM@users.noreply.github.com>
2022-03-26 17:17:23 +0530

    Add files via upload

--- content_view.swift
+++ content_view.swift
@@ -1,77 +1,81 @@
+//
+//  contacts.swift
+//  terra
+//
+//  Created by Varun Satheesh on 26/03/22.
+//
+
 import SwiftUI
 
-struct ContentView: View {
+struct contacts: View {
+   @State var rooms:[Room]=[]
     var body: some View {
+        
+        
         NavigationView{
-            
-            VStack {
-                HStack {
-                    
-                    Button(action: {}) {
-                        Image(systemName: "phone.circle.fill")
-                            .foregroundColor(Color.black)
+            VStack{
+                Button(action: {rooms.append(Room(name:"New1", number :"xxxxxxxxxxx", image: "contact1"))})
+            {Text("Add Contact")}
+            List(rooms) { room in
+                
+                if room.hasPic{
+                    HStack(alignment: .center){
+                        Image("contact1" )
+                            .resizable()
+                            .scaledToFit()
                             
+                            .cornerRadius(8)
+                            .padding(15)
+                    VStack(alignment:
+                            .leading) {
                         
-                            .font(.system(size: 56.0))
+                            
+                        Text(room.name)
+                            .font(.system(size: 50))
+                        Text("\(room.number) ").foregroundColor(.secondary)
+                            .font(.system(size: 30))
+                    
+                    
+                }
                     }
-                    .padding(50)
-                    .background(Color.orange)
-                    .clipShape(Rectangle())
-                    .cornerRadius(12)
+                }
+                else{
+                    HStack(alignment: .center){
+                        Image(systemName: room.image )
+                            .alignmentGuide(.bottom) { d in d[.bottom] * 0.5 }
+                            .cornerRadius(8)
+                            .padding(50)
+                    VStack(alignment:
+                            .leading) {
+                        
+                            
+                        Text(room.name)
+                            .font(.system(size: 50))
+                        Text("\(room.number) ").foregroundColor(.secondary)
+                            .font(.system(size: 30))
                     
                     
-                    
-                    Button(action: {}) {
-                        Image(systemName: "exclamationmark.circle.fill")
-                            .foregroundColor(Color.black)
-                            .font(.system(size: 56.0))
-                    }.padding(50)
-                        .background(Color.orange)
-                        .clipShape(Rectangle())
-                        .cornerRadius(12)
-                }.padding()
+                }
+                    }
+                }
+            
                 
                 
-                HStack {
-                    
-                    Button(action: {}) {
-                        Image(systemName: "line.3.horizontal.circle.fill")
-                            .foregroundColor(Color.black)
-                            .font(.system(size: 56.0))
-                    }.padding(50)
-                        .background(Color.orange)
-                        .clipShape(Rectangle())
-                        .cornerRadius(12)
-                    
-                    
-                    Button(action: {}) {
-                        Image(systemName: "apps.iphone")
-                            .foregroundColor(Color.black)
-                            .font(.system(size: 56.0))
-                            .padding(0.5)
-                    }.padding(50)
-                        .background(Color.orange)
-                        .clipShape(Rectangle())
-                        .cornerRadius(12)
-                }.padding()
-                
-                Button(action: {}) {
-                    Image(systemName: "person.wave.2")
-                        .foregroundColor(Color.white)
-                        .font(.system(size: 56.0))
-                }.padding(20)
-                    .background(Color.black)
-                    .clipShape(Rectangle())
-                    .cornerRadius(12)
-                
+            
+        }
+        .navigationBarTitle(Text("Contacts"))
+            
+            
             }
-            .navigationBarTitle(Text("Boomer"))
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+#if DEBUG
+struct contacts_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        contacts(rooms: testData)
+.previewInterfaceOrientation(.portrait)
     }
 }
+#endif
